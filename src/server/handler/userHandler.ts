ebf0a715ff149d597c62ccc12793c528910d92a9
Konrad <konrad.hartwig@online.de>
2020-05-24 14:57:34 -0700

    Delete singIn and signUp

--- src/server/handler/userHandler.ts
+++ src/server/handler/userHandler.ts
@@ -8,39 +8,8 @@ export default class UserHandler extends Handler {
 
     constructor() {
         super();
-        this.getRouter().post("/signUp", this.signUp.bind(this));
-        this.getRouter().post("/signIn", this.signIn.bind(this));
         this.getRouter().get("/user", this.getUser.bind(this));
         this.getRouter().get("/logout", this.logout.bind(this));
-    }
-
-    private async signUp(req: express.Request, res: express. Response): Promise<void> {
-        try {
-            if (await User.findOne({ email: req.body.email }))
-                throw new Error("Email is already in use");
-            req.body.password = utils.hashPassword(req.body.password);
-            const user = await User.create(req.body);
-            res.json({
-                "status": "success",
-                "token": user._id
-            });
-        } catch (e) {
-            utils.handleError(e, res);
-        }
-    }
-
-    private async signIn(req: express.Request, res: express. Response): Promise<void> {
-        try {
-            const user = await User.findOne({ email: req.body.email, password: utils.hashPassword(req.body.password) });
-            if (!user)
-                throw new Error("Invalid login data");
-            res.json({
-                "status": "success",
-                "token": user._id
-            });
-        } catch (e) {
-            utils.handleError(e, res);
-        }
     }
 
     private async getUser(req: express.Request, res: express.Response): Promise<void> {
